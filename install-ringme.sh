29a58bb971bf1f04d812179a07728a61ecc06386
phatIntern <phat.pham@saleshood.com>
2023-03-21 05:35:51 +0700

    first commit

---
+++ install-ringme.sh
@@ -0,0 +1,25 @@
+#!/bin/bash
+
+# Define the name of the new command
+COMMAND_NAME="ringme"
+
+# Define the path to the directory where the new command will be installed
+INSTALL_DIR="/usr/local/bin"
+
+# Define the URL of the script that implements the new command
+SCRIPT_URL="https://example.com/mynewcommand.sh"
+
+# Check if the command already exists in the installation directory
+if [ -f "$INSTALL_DIR/$COMMAND_NAME" ]; then
+    echo "Error: $COMMAND_NAME already exists in $INSTALL_DIR. Aborting installation."
+    exit 1
+fi
+
+# Download the command script from the URL and write it to the installation directory
+curl -sSL "$SCRIPT_URL" > "$INSTALL_DIR/$COMMAND_NAME"
+
+# Set the correct permissions for the new command
+chmod +x "$INSTALL_DIR/$COMMAND_NAME"
+
+# Display a message indicating that the installation was successful
+echo "Installation of $COMMAND_NAME complete. You can now use '$COMMAND_NAME echo neat' to try out the command."
---
+++ pingme.sh
@@ -0,0 +1,6 @@
+function pingme() {
+    # Call the devenv shell with the command
+    $@
+
+    afplay elevator-door-open.m4a
+}
