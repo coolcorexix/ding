4553d402aad2987820ce00209515ef3b039fba37
phatIntern <phat.pham@saleshood.com>
2023-03-28 10:15:52 +0700

    update command for more robust accross app

--- ding.sh
+++ ding.sh
@@ -1,16 +1,17 @@
-# Call the devenv shell with the command
+#!/bin/zsh
+
 $@
 
 # if the command is run on mac use this script
 if [ "$(uname)" == "Darwin" ]; then
     # play the sound
-    afplay /usr/local/bin/elevator-door-open.m4a
+    afplay /usr/local/bin/elevator-door-open.wav
 fi
 
 # if the command is run on linux use this script
 if [ "$(uname)" == "Linux" ]; then
     # play the sound
-    aplay /usr/local/bin/elevator-door-open.m4a
+    aplay /usr/local/bin/elevator-door-open.wav
 fi
 
 
