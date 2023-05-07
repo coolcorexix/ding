30a668053ebd1973d351b2b0ea4ad760b612b358
phatIntern <phat.pham@saleshood.com>
2023-05-08 06:43:55 +0700

    feat: add buzz sound for error case

---
+++ command-that-fail.sh
@@ -0,0 +1,2 @@
+# a command that will exit with a non-zero exit code
+exit 1
--- ding.sh
+++ ding.sh
@@ -1,18 +1,29 @@
 #!/bin/bash
 $@
 
-# if the command is run on mac use this script
-if [[ "$(uname)" == "Darwin" ]]; then
+# get the exit code of the last command
+exit_code=$?
+
+# define a function to play the sound with filename as param of function
+function play_sound {
+    # if the command is run on mac use this script
+    if [[ "$(uname)" == "Darwin" ]]; then
+        # play the sound
+        afplay /usr/local/bin/$1
+    fi
+
+    # if the command is run on linux use this script
+    if [[ "$(uname)" == "Linux" ]]; then
+        # play the sound
+        aplay /usr/local/bin/$1
+    fi
+}
+
+if [[ $exit_code -ne 0 ]]; then
     # play the sound
-    afplay /usr/local/bin/elevator-door-open.wav
+    play_sound buzz-sound.wav
+    # stop the script right here
+    exit $exit_code
 fi
 
-# if the command is run on linux use this script
-if [[ "$(uname)" == "Linux" ]]; then
-    # play the sound
-    aplay /usr/local/bin/elevator-door-open.wav
-fi
-
-
-
-
+play_sound elevator-door-open.wav
